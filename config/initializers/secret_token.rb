c50d1614cde29c2a01826680576aaa09cb9ffbb5
Kevin Klapak <kevin.klapak@gmail.com>
2013-09-05 14:02:47 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IndieMusic::Application.config.secret_key_base = '3070e556efe9af1886864ab5e746f4026a4a28bac209fe27e126a5435d0dc1aa8ded19d0c6db03dc2efd145182e6da1fe61069c0b6e834ad1117ba52e71c086c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IndieMusic::Application.config.session_store :cookie_store, key: '_indie_music_session'
