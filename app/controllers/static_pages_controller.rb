e3c12cd93bfdc8b1ee9e1ce76c2292312cb2ee02
Kevin Klapak <kevin.klapak@gmail.com>
2013-09-12 13:56:03 -0700

    Add profile

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,16 @@
 class StaticPagesController < ApplicationController
-  def home
+  def signup
+  	if user_signed_in?
+    	redirect_to :controller=>'static_pages', :action => 'home'
+   	end
   end
 
   def about
   end
 
   def help
+  end
+
+  def home
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 IndieMusic::Application.routes.draw do
   devise_for :users
 
-  root  'static_pages#home'
+  root  'static_pages#signup'
+  match '/profile', to:'users#profile',   		via: 'get'
   match '/signup',  to: 'users#new',   		 	via: 'get'
+  match '/home',  	to:'static_pages#home',   		via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
